1a27b9cfb49e6f868247a58997ecc2b391b7840e
dehai.wang <dehai.wang@justcoglobal.com>
2026-04-19 22:08:11 +0800

    init.

---
+++ src/test/java/com/dreamroute/monitor/SendTest.java
@@ -0,0 +1,32 @@
+package com.dreamroute.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+@ActiveProfiles("local")
+class SendTest {
+
+    @Resource
+    private JavaMailSender javaMailSender;
+
+    @Test
+    void mmTest() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        // 发件人
+        message.setFrom("342252328@qq.com");
+        // 收件人(可实现批量发送)
+        message.setTo("342252328@qq.com");
+        // 邮箱标题
+        message.setSubject("腾讯新闻");
+        // 邮箱内容
+        message.setText("腾讯新闻");
+        // 发送邮件
+        javaMailSender.send(message);
+    }
+}
